d0dfa81594d0f67acb9005587cb5133a7841ce83
Alfred Cepeda <AlfredJCepeda@gmail.com>
2016-08-03 07:20:30 +0000

    Container tests pass again

--- tests/container.js
+++ tests/container.js
@@ -6,17 +6,24 @@ var xtape = function(name) {
 	console.log('Manuall skipped:', name);
 };
 
-xtape('exec', function(t) {
-	var command = new Command('aljcepeda/debian', 'latest', [ 'uname', { 'flags':'mrs' } ]);
+tape('exec', function(t) {
+	var command = new Command('aljcepeda', 'php', 'latest', [
+		'--rm'
+	], 'php', [
+		'-r',
+		'while(true) { $moo = 1; echo $moo; }'
+	]);
+
+	var command = new Command('aljcepeda', 'debian', 'latest', ['--rm'], 'uname', ['-mrs']);
 	var cmd = command.build('run');
 
-	t.equal(
+	t.deepEqual(
 		cmd,
-		'sudo docker run aljcepeda/debian:latest uname -mrs',
+		[ 'sudo', 'docker', 'run', '--rm', 'aljcepeda/debian:latest', 'uname', '-mrs' ],
 		'Generates docker command'
 	);
 
-	var container = new Container(cmd);
+	var container = new Container(command);
 	container.exec().then(function(result) {
 		t.equal(
 			result.stdout,
